fa08dbd2bc00ac707ada8c28d54a0101237754a1
jsdmas <sdmas5730@gmail.com>
2023-09-04 17:33:44 +0900

    feat : 기본 page 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return (
+    <div className='w-full max-w-xl mx-auto'>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default App;
