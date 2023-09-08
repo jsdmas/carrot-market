ac758ba3e83ccd1a067cfd1f487fcff88df7d710
jsdmas <sdmas5730@gmail.com>
2023-09-08 12:09:31 +0900

    fix : Link a 태그 수정 및 api 폴더 추가

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@/libs/client';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await client.user.create({
+    data: {
+      name: 'hello2',
+      email: 'hello2@naver.com',
+    },
+  });
+  res.json({
+    ok: 200,
+  });
+}
