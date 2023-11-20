44a37df9e89cdda611ad98c11f1976b6d9050e7c
jsdmas <sdmas5730@gmail.com>
2023-11-20 16:06:29 +0900

    feat : server, client

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Button from '../components/button';
 import Input from '../components/input';
-import { cls } from '../libs/utils';
+import { cls } from '../libs/client/utils';
+import useMutation from '@/libs/client/useMutation';
 
 interface EnterForm {
   email?: string;
@@ -11,7 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
-  const [submitting, setSubmitting] = useState(false);
+  const [enter, { loading, data, error }] = useMutation('/api/users/enter');
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<'email' | 'phone'>('email');
   const onEmailClick = () => {
@@ -22,17 +23,9 @@ const Enter: NextPage = () => {
     reset();
     setMethod('phone');
   };
-  const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch('/api/users/enter', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then(() => {
-      setSubmitting(false);
-    });
+  const onValid = (validForm: EnterForm) => {
+    if (loading) return;
+    enter(validForm);
   };
   return (
     <div className='mt-16 px-4'>
@@ -68,8 +61,8 @@ const Enter: NextPage = () => {
             />
           ) : null}
           {method === 'phone' ? <Input register={register('phone')} name='phone' label='Phone number' type='number' kind='phone' required /> : null}
-          {method === 'email' ? <Button text={'Get login link'} /> : null}
-          {method === 'phone' ? <Button text={submitting ? 'Loading' : 'Get one-time password'} /> : null}
+          {method === 'email' ? <Button text={loading ? 'Loading' : 'Get login link'} /> : null}
+          {method === 'phone' ? <Button text={loading ? 'Loading' : 'Get one-time password'} /> : null}
         </form>
 
         <div className='mt-8'>
