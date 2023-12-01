892082eb965e45923fdffa45a5176d912275f0a4
jsdmas <sdmas5730@gmail.com>
2023-12-01 17:33:48 +0900

    7.1 Authentication

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@/libs/server/withHandler';
+import client from '@/libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(401).end();
-  }
-  console.log(req.body.email);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
